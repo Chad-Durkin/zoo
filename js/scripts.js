ecc0cd95aea08bb859e807ead2383a6c9f3af0ea
Chad Durkin and Caitlin Hines <chad@>
2017-01-10 15:15:10 -0800

    toggle background images and header

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,22 @@ $(document).ready(function() {
 $("#lion").click(function() {
   $("#lionpic").fadeToggle();
 });
+
+$("#lionpic").click(function() {
+  $("p#roar").fadeToggle();
+  $("body").toggleClass("lionBG")
+  $("h1").toggleClass("h1lion")
+});
+
+$("#tigerpic").click(function() {
+  $("body").toggleClass("tigerBG");
+  $("h1").toggleClass("h1tiger");
+});
+
 $("#tiger").click(function() {
   $("#tigerpic").fadeToggle();
 });
 $("#bear").click(function() {
   $("#bearpic").fadeToggle();
 });
-
 });
