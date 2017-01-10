9e2bd38be41f9ce58efd704222408780aa68420b
Chad Durkin and Caitlin Hines <chad@>
2017-01-10 14:07:10 -0800

    All animals images toggle

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$("#lion").click(function() {
+  $("#lionpic").fadeToggle();
+});
+$("#tiger").click(function() {
+  $("#tigerpic").fadeToggle();
+});
+$("#bear").click(function() {
+  $("#bearpic").fadeToggle();
+});
+
+});
